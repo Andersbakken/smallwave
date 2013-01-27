4b397970105fedae6f23eb8bca2cfb59d383aecd
Anders Bakken <agbakken@gmail.com>
2013-01-26 21:55:18 -0800

    add script

---
+++ script.js
@@ -0,0 +1,65 @@
+var fs = require("fs");
+var util = require("util");
+
+var symbols = {
+    "main.cpp": [
+        { "location": { "file": "main.cpp", "offset": 5, "length": 3 },
+          "type": "FunctionDecl" },
+        { "location": { "file": "main.cpp", "offset": 21, "length": 4 },
+          "type": "FunctionDecl" },
+        { "location": { "file": "main.cpp", "offset": 34, "length": 3 },
+          "type": "DeclRefExpr",
+          "target": { "file": "main.cpp", "offset": 5, "length": 3 }
+        },
+    ],
+};
+
+function onload()
+{
+    // console.log(util.inspect(symbols, false, null)); //JSON.stringify(symbols));
+
+    var src = fs.readFileSync("main.cpp").toString();
+    // var src = document.getElementById("src").innerHTML;
+    // console.log("Foo " + src);
+    var symbolIndex = 0;
+    var srcIndex = 0;
+    var out = "";
+    function processSymbol(symbol)
+    {
+        if (srcIndex < symbol.location.offset) {
+            out += src.substring(srcIndex, symbol.location.offset).replace(/ /g, "&nbsp;");
+            out += "<a id=\"" + symbol.location.offset + "\"";
+            if (symbol.target) {
+                out += " href=\"" + symbol.target.file + ".html#" + symbol.target.offset + "\"";
+            }
+            out += ">";
+            out += src.substr(symbol.location.offset, symbol.location.length).replace(/ /g, "&nbsp;");
+            out += "</a>";
+            srcIndex = symbol.location.offset + symbol.location.length;
+        }
+    }
+    for (var file in symbols) {
+        symbols[file].forEach(processSymbol);
+    }
+    out += src.substr(srcIndex).replace(/ /g, "&nbsp;");
+    // for (var i=0; i<symbols["
+    // while (i < src.length) {
+    //     if (
+
+    // }
+    out = out.replace(/\n/g, "<br/>\n");
+    // out = out.replace(/ /g, "&nbsp;");
+    console.log("<html>\n" + out + "</html>");
+}
+onload();
+
+// var src = "void foo()\n"
+//         + "{\n"
+// + "}\n"
+
+
+// int main()
+// {
+//     foo();
+//     return 0;
+// }
